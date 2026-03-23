f2788d211460cea69e9d7a730590ad5a96ff3d9f
Mykhailo Moskalov <misha@moskalov.com>
2026-03-23 23:30:40 +0100

    fix legal

--- app/legal/page.tsx
+++ app/legal/page.tsx
@@ -1,11 +1,11 @@
-// import Container from "@/components/Container/Container";
+import Container from "@/components/Container/Container";
 // import { Router } from "next/router";
 
-// export default function Home() {
+export default function Home() {
 //   const router = new Router();
-//   return (
-//     <Container>
-//       <h1 style={{ color: "#000", marginTop: 140 }}>Impressum</h1>
-//     </Container>
-//   );
-// }
+  return (
+    <Container>
+      <h1 style={{ color: "#000", marginTop: 140 }}>Impressum</h1>
+    </Container>
+  );
+}
